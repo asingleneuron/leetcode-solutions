b0a0b44a3124380791a37b1738fb6f9e0449d3e8
shobhit upadhyaya <52558721+asingleneuron@users.noreply.github.com>
2020-05-04 14:26:50 +0530

    Added explanation

--- may_leetcode_challenge/Day_4/number_complement.py
+++ may_leetcode_challenge/Day_4/number_complement.py
@@ -1,5 +1,24 @@
 class Solution:
 
     def findComplement(self, num: int) -> int:
+        '''
+
+        :param num:
+        :return: num
+
+        Example:
+            num = 5
+
+            int("".join(map(lambda x: '0' if x == '1' else '1', bin(num)[2:])), 2)
+
+            breakup of one-liner
+
+            binary_bits = bin(num)[2:]  # Convert number to ints binary representation.
+
+            rev_bits = ''.jon(map(lambda x: '0' if x == '1' else '1' , binary_bits)  # Reversing 1 and 0
+
+            int(rev_bits, 2)    # Converting binary representation to decimal
+
+        '''
         return int("".join(map(lambda x: '0' if x == '1' else '1', bin(num)[2:])), 2)
 
